12c7ca2869f38adf101a7a7c74674167a8aa89ad
Peter Marklund <peter@marklunds.com>
2026-02-21 10:48:43 +0100

    first commit, basic working script

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # json-cli
-A convenient way to work with JSON using JavaScript in the terminal
+
+An npm package that provides a convenient way to work with JSON using JavaScript in the terminal.
+
+## TODO
+
+* Use stable/sorted JSON stringify
+
+# Prior Art
+
+* [trentm/json](https://github.com/trentm/json) - nice library with [very good documentation](https://trentm.com/json/)
---
+++ bin/json.js
@@ -0,0 +1,152 @@
+#!/usr/bin/env node --max-old-space-size=16384
+
+// Simple node script that provides a jq alternative for processing JSON using JavaScript on the command line.
+// Supports newline separated JSON (JSON lines) as well as JSON data.
+// Also supports log data where some lines are JSON and some aren't and log lines where the
+// beginning of the line is text and the end of the line is JSON, i.e.:
+//
+// 2022-10-18T14:07:53.960Z [INFO ] starting server with config: {"port":3000}
+
+// With pipe (can break for large amounts of JSON data)
+//
+// cat some-data.json | json <javascript-code>
+//
+// With a file path (works better for large amounts of data):
+//
+// json <javascript-code> <file-path>
+// The JSON data is available in a `data` variable. If the JavaScript code argument starts with a "." then that is equivalent to starting it with "data.".
+//
+// You can use map and filter and useful lodash functions like get:
+//
+// cat some-data.json | json ".Items.map(i => ({id: get(i, 'id.S'), updatedAt: get(i, 'updatedAt.N')})).filter(i => new Date(Number(i.updatedAt)) < new Date())"
+//
+// You can access the JSON data with the data variable:
+//
+// cat some-data.json | json "Object.keys(data)"
+//
+// Split complex processing with pipes if it helps readability:
+//
+// cat some-data.json \
+//  | json ".Items.map(i => ({id: get(i, 'id.S'), updatedAt: get(i, 'updatedAt.N')}))" \
+//  | json ".filter(i => new Date(Number(i.updatedAt)) < new Date())"
+//
+// Easily print lengths of arrays or keys of objects etc:
+//
+// cat some-data.json | json ".Items.length"
+//
+// Pretty print (echo) JSON data:
+//
+// cat some-data.json | json .
+
+const fs = require("fs");
+const readline = require("readline");
+const _ = require("lodash");
+Object.assign(global, require("lodash"));
+const { diff } = require("object-diffy");
+const { colorize } = require("json-colorizer");
+
+function getCodeArg() {
+    let code = process.argv[2] || "data"
+    // Support jq like dot syntax
+    if (code === ".") {
+        code = "data"
+    } else if (code.startsWith(".")) {
+        code = "data" + code
+    }
+    return code
+}
+
+function readStdIn() {
+    return fs.readFileSync(0).toString();
+}
+  
+async function jsonIn(filePath) {
+    let textInput
+    try {
+      if (filePath) {
+        return require(filePath)
+      } else {
+        textInput = readStdIn()
+        // NOTE: I've found JSON.parse intermittently errors out for data sizes around 15 MB but require(filePath) can handle more?
+        // const dataSizeMb = Buffer.byteLength(textInput) / 1024 / 1024
+        return JSON.parse(textInput)
+      }
+    } catch (jsonErr) {
+      try {
+        const lines = []
+        let openLines = []
+        let nErrors = 0
+        let lineCount = 0
+        const processLine = (line) => {
+          lineCount += 1
+          const result = parseLine(line, openLines)
+          if (result.error) {
+            console.log(error)
+            nErrors += 1
+          }
+          if (result.parsedLine) lines.push(result.parsedLine)
+          openLines = result.openLines
+        }
+        if (filePath) {
+          const rl = readline.createInterface({
+            input: fs.createReadStream(filePath),
+            crlfDelay: Infinity,
+          })
+          for await (const line of rl) {
+            processLine(line)
+          }
+        } else {
+          textInput = textInput || readStdIn()
+          const textLines = textInput
+            .trim()
+            .split("\n")
+            .map((l) => l.trim())
+            .filter(Boolean)
+          for (const [index, line] of textLines.entries()) {
+            processLine(line)
+          }
+        }
+        if (nErrors > 0)
+          console.log(
+            `Failed to parse ${nErrors}/${textLines.length} lines due to errors`
+          )
+        return lines
+      } catch (linesErr) {
+        console.log(jsonErr.stack)
+        console.log(linesErr.stack)
+        throw new Error("Could not parse input as JSON or as JSON lines")
+      }
+    }
+}
+  
+function printJson(data) {
+    console.log(JSON.stringify(data))
+}
+  
+function printPrettyJson(data) {
+    console.log(colorize(JSON.stringify(data, null, 4)))
+}
+
+async function main() {
+    const code = getCodeArg()
+    const filePath = process.argv[3]
+  
+    const data = await jsonIn(filePath)
+  
+    const processedData = eval(code)
+  
+    if (process.env.RAW === "true" || (process.env.RAW !== "false" && ["string", "number", "boolean"].includes(typeof processedData))) {
+      console.log(processedData)
+    } else if (process.env.JSONL === "true" && Array.isArray(processedData)) {
+      printJsonLines(processedData)
+    } else if (process.env.PRETTY === "false") {
+      printJson(processedData)
+    } else {
+      printPrettyJson(processedData)
+    }
+}
+  
+
+if (require.main === module) {
+  main()
+}
